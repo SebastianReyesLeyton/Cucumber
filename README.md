27c309f8a356a75176effe7f57520eb3b97e3626
SebastianReyesLeyton <sebas.reyes2002@hotmail.com>
2023-03-23 22:36:42 -0500

    [basic_tutorial] Changes: - Update Node.js version to 18 - Add name to cucumber_test service images - Modify the package.json scripts to execute cucumber - Create README.md file to store important documentation - Create the first cucumber configuration

---
+++ README.md
@@ -0,0 +1,10 @@
+# Cucumber
+
+This project is to learn about cucumber.
+
+## Restrictions
+
+1. Cucumber works with Node.js versions: 14 || 16 || 18>=.
+2. To run cucumber tests, you can use the next 2 commands:
+    - `npx cucumber-js`: native form
+    - `npm test`: in this case you need setup your `package.json` file in the script `"test"` option, change the current value to `node ./node_modules/@cucumber/cucumber/bin/cucumber-js ./features`
--- commands.sh
+++ commands.sh
@@ -8,9 +8,35 @@ bash () {
     docker-compose run --rm $1 sh
 }
 
+# Build project images
+build () {
+    docker-compose build
+}
+
+options () {
+    echo -e "\n"
+
+    echo -e "Please choose one of the next options:"
+    echo -e "COMMAND \t DESCRIPTION"
+    echo -e "bash    \t run docker service container into terminal"
+    echo -e "build   \t build docker-compose images"
+
+    echo -e "\n"
+
+    echo -e "This are the args by each command:"
+    echo -e "COMMAND \t ARGUMENTS"
+    echo -e "bash    \t <service> docker service name that you want to run"
+    echo -e "build"
+
+}
+
 main () {
     if [[ "$COMMAND" == "bash" ]]; then
         bash $ARGS
+    elif [[ "$COMMAND" == "build" ]]; then
+        build
+    else
+        options
     fi
 }
 
