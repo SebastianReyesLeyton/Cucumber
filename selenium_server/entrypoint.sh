4937a33a5f7e46a8ec6a4f97ee0ef03c13a26909
SebastianReyesLeyton <sebas.reyes2002@hotmail.com>
2023-04-03 19:52:15 -0500

    [selenium_server] Changes: - Resolve files conflic because of windowns system

--- selenium_server/entrypoint.sh
+++ selenium_server/entrypoint.sh
@@ -1,7 +1,6 @@
-
 # Run vnc
 echo -e "Configure xvfb"
-bash /root/.scripts/xvfb-entrypoint.sh > /dev/null 2>&1 &
+/root/.scripts/xvfb-entrypoint.sh > /dev/null 2>&1 &
 
 echo -e "Configure vnc"
 bash /root/.scripts/vnc-entrypoint.sh > /dev/null 2>&1 &
@@ -9,8 +8,5 @@ bash /root/.scripts/vnc-entrypoint.sh > /dev/null 2>&1 &
 echo -e "Configure novnc"
 bash /root/.scripts/no-vnc-entrypoint.sh > /dev/null 2>&1 &
 
-# Execute selenium server
-java -Dwebdriver.http.factory=jdk-http-client \
-     -jar ${CUCUMBER_JSRL_SELENIUM_SERVER_FILE} \
-     --ext ${CUCUMBER_JSRL_SELENIUM_HTTP_JDK_CLIENT_FILE} standalone \
-     --config /usr/src/selenium-server/config.toml 
+# Execute selenium
+java -Dwebdriver.http.factory=jdk-http-client -jar ${CUCUMBER_JSRL_SELENIUM_SERVER_FILE} --ext ${CUCUMBER_JSRL_SELENIUM_HTTP_JDK_CLIENT_FILE} standalone --config /usr/src/selenium-server/config.toml
